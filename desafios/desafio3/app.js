811b07323d3d88791ff69685b4c1d1a56ff24df4
Francisco Guzman <guzman.franciisco@gmail.com>
2024-04-28 21:17:09 -0300

    agregue try and catch

--- desafios/desafio3/app.js
+++ desafios/desafio3/app.js
@@ -6,28 +6,35 @@ const PORT = 8080;
 
 app.use(express.json());
 
-const productManager = new ProductManager(); 
+const productManager = new ProductManager();
 
 app.get("/products", (req, res) => {
-    const limit = req.query.limit;
-    if (limit) {
-        const limitedProducts = productManager.getProducts().slice(0, limit);
-        res.json(limitedProducts);
-    } else {
-        res.json(productManager.getProducts());
+    try {
+        const limit = req.query.limit;
+        if (limit) {
+            const limitedProducts = productManager.getProducts().slice(0, limit);
+            res.json(limitedProducts);
+        } else {
+            res.json(productManager.getProducts());
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error al procesar la solicitud" });
     }
 });
 
-app.get("/products/:pid", (req, res)=>{
-    const productId = parseInt(req.params.pid)
-    const product = productManager.getProductById(productId)
-    if(product){
-        res.json(product)
-    }else{
-        res.status(404).json({messege: "Producto no encontrado"})
+app.get("/products/:pid", (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+        const product = productManager.getProductById(productId);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ message: "Producto no encontrado" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error al procesar la solicitud" });
     }
-})
-
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor Express corriendo en el puerto ${PORT}`);
